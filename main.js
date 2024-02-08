d99a65a27f37b130a04ebacf24a20a07f8dbc150
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-08 18:32:46 +0600

    delete invalids function done

--- main.js
+++ main.js
@@ -53,24 +53,6 @@ function deleteInvalids(array) {
    return validArray;
 }
 
-console.log(deleteInvalids([NaN, 1, 12, 0, -1, "hello", undefined]));
-console.log(
-   deleteInvalids([
-      1,
-      null,
-      undefined,
-      18,
-      -19,
-      NaN,
-      "12",
-      [1, 2],
-      { ob: "lala" },
-   ])
-);
-console.log(deleteInvalids(["1", { num: 2 }, NaN]));
-console.log(deleteInvalids([1, 2, -3]));
-console.log(deleteInvalids({ num: [1, 2, 3] }));
-
 function password(obj) {
    const name = obj.name;
    const birthYear = obj.birthYear;
